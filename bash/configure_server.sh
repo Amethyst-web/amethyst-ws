ff0cf9c53ba75faece22b8192ee0f69b6c768d11
HEKET313 <neket313@gmail.com>
2016-01-06 14:59:39 +0300

    Поправил ещё немного скрипт

--- bash/configure_server.sh
+++ bash/configure_server.sh
@@ -2,15 +2,16 @@
 
 #ставим весь необходимый софт
 apt-get update
-apt-get install git-core
-apt-get install nodejs
-apt-get install npm
+apt-get install git-core nodejs npm
+ln -s /usr/bin/nodejs /usr/bin/node
 npm install -g bower
 npm install -g less
+npm install -g less-plugin-clean-css
 
 #конфигурим git
 git config --global user.email "neket313@gmail.com"
 git config --global user.name "HEKET313"
+git config --global color.ui true
 
 #разворачиваем приложение
 cd /var/www
