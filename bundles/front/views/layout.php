eb6ebc25fd576c4be17048b88af29ba9e5d619f9
HEKET313 <neket313@gmail.com>
2016-01-06 13:57:16 +0300

    Добавил пару служебных bash-скриптов для настройки сервера и разворачивания приложения + поменял немного htaccess для нормального обращения к статике

--- bundles/front/views/layout.php
+++ bundles/front/views/layout.php
@@ -4,5 +4,14 @@
  * Date: 19.11.2015
  * Time: 18:47
  */ ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Веб-студия Аметист</title>
+    <link rel="stylesheet" href="/assets/css/style.min.css">
+</head>
+<body>
 
+</body>
+</html>
 <?php include $content; ?>
