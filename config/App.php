7227aab8cdf27a63fb168f39dc2762ae5e0adecf
HEKET313 <neket313@gmail.com>
2016-01-08 00:23:12 +0300

    Добавил

---
+++ config/App.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nikita
+ * Date: 19.11.2015
+ * Time: 19:17
+ */
+
+namespace config;
+
+class App
+{
+    const SUPPORT_NAME = 'Amethyst Web Studio';
+
+    const EMAIL_REGEXP = '/^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i';
+    const PHONE_REGEXP = '/^(\+7|8)[0-9]{10}$/';
+}
